76eaadf1f7b8e02f5a110630082037e499545fe2
petar_plecas <plecaspetar3@gmail.com>
2019-03-17 17:56:36 +0100

    Added authentication

--- DragonStack/frontend/src/index.js
+++ DragonStack/frontend/src/index.js
@@ -3,9 +3,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
-import Generation from './components/Generation';
-import Dragon from './components/Dragon';
 import rootReducer from './reducers';
+import Root from './components/Root';
+import { fetchdAuthenicated } from './actions/account';
 import './index.css';
 
 
@@ -16,13 +16,13 @@ const store = createStore(
   applyMiddleware(thunk)
   );
 
-render(
-  <Provider store= {store}>
-    <div>
-      <h2>Dragon Stack</h2>
-      <Generation />
-      <Dragon />
-    </div>
-  </Provider>,
-  document.getElementById('root')
-);
+store.dispatch(fetchdAuthenicated())
+  .then(() => {
+    render(
+      <Provider store= {store}>
+        <Root />
+      </Provider>,
+      document.getElementById('root')
+    );
+  });
+
